1342402ddf921045c762c8e092752600243674d9
Ggs91 <georges_atalla@hotmail.fr>
2020-10-19 22:04:51 +0200

    adding uttons to card component

--- src/assets/stylesheets/js/main.js
+++ src/assets/stylesheets/js/main.js
@@ -51,9 +51,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const cardOverlays = document.querySelectorAll('.card-overlay');
   const cardImageWrappers = document.querySelectorAll(".card-img-wrapper");
   const cardTitles = document.querySelectorAll(".card-title");
-  const closeDetailsButtons = document.querySelectorAll('.close-details');
-
-  
+  const closeDetailsButtons = document.querySelectorAll('.close-overlay');
 
   for (let i = 0; i < cardOverlays.length; i++) {
     cardTitles[i].addEventListener("click", ()=> cardOverlays[i].classList.add('show'));
