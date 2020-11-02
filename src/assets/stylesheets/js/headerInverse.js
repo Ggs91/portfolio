4c1bad7adcddcff2d36e8ae81dd4c7bb74460536
Ggs91 <georges_atalla@hotmail.fr>
2020-11-02 22:18:46 +0100

    fixing header switching color on scroll issue

--- src/assets/stylesheets/js/headerInverse.js
+++ src/assets/stylesheets/js/headerInverse.js
@@ -1,45 +1,23 @@
 ////////////////////// Observers //////////////////////
-//observers to make the logo and button menu (in the header) change color when they intersect with dark sections
+//observers to make the logo and menu button (in the header) switch color when they intersect with white/dark sections
 const header = document.querySelector(".primary-header");
 
-// Hero Section Observer
 const heroSection = document.querySelector("section#hero");
 const heroSectionOptions = {
   root: null,
-  rootMargin: "-6% 0px 0px 0px"
+  rootMargin: "-5% 0px 0px 0px"
 };
 
 const heroSectionObserver = new IntersectionObserver(function(entries, heroSectionObserver) {
   entries.forEach(entry => {
-    console.log(entry);
     if (!entry.isIntersecting) {
-      header.classList.add("inverse");
+      console.log("IF");
+      header.classList.add("inverse"); // switch to black header
     } else {
-      header.classList.remove("inverse");
+      console.log("ELSE");
+      header.classList.remove("inverse"); // switch back to white header
     }
   });
 }, heroSectionOptions)
 
-heroSectionObserver.observe(heroSection)
-
-// Footer Observer
-
-const footer = document.getElementById("primary-footer");
-
-const footerOptions = {
-  root: null,
-  rootMargin: "0px 0px -94% 0px"
-};
-
-const footerObserver = new IntersectionObserver(function(entries, footerObserver) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      header.classList.add("inverse");
-    } else {
-      header.classList.remove("inverse");
-    }
-  });
-
-}, footerOptions)
-
-footerObserver.observe(footer)
+heroSectionObserver.observe(heroSection);
